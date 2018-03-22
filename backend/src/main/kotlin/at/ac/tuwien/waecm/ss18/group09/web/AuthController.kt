83cad64fdb1eed9ccb1f0480bdfc3353e1ed02c3
Daniel Füvesi <e1326576@student.tuwien.ac.at>
2018-03-22 19:28:35 +0100

    resolve ktlint issue about wildcard imports

--- backend/src/main/kotlin/at/ac/tuwien/waecm/ss18/group09/web/AuthController.kt
+++ backend/src/main/kotlin/at/ac/tuwien/waecm/ss18/group09/web/AuthController.kt
@@ -7,7 +7,11 @@ import org.springframework.http.MediaType
 import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.userdetails.User
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import reactor.core.publisher.Mono
 
 /**
