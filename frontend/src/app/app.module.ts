36f5bbc09bc4e7140d5ad3c7fabdcc4870eb65d4
Michael Sober <michael.sober@ymail.com>
2018-04-15 14:02:00 +0200

    Added form validation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from "./app-routing.module";
@@ -24,6 +24,7 @@ import { RegisterComponent } from './register/register.component';
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../model/user";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -11,14 +12,51 @@ export class RegisterComponent implements OnInit {
   user: User;
   genders = ["Male", "Female"];
 
+  registerForm: FormGroup;
+  email: FormControl;
+  password: FormControl;
+  name: FormControl;
+  gender: FormControl;
+  birthday: FormControl;
+
   constructor() { }
 
   ngOnInit() {
     this.user = new User();
+    this.email = new FormControl(
+      this.user.email,
+      [Validators.required, Validators.email]
+    );
+    this.password = new FormControl(
+      this.user.password,
+      [Validators.required, Validators.minLength(8)]
+    );
+    this.name = new FormControl(
+      this.user.name,
+      [Validators.required, Validators.minLength(4)]
+    );
+    this.gender = new FormControl(
+      this.user.gender,
+      [Validators.required]
+    );
+    this.birthday = new FormControl(
+      this.user.birthday,
+      [Validators.required]
+    );
+    this.registerForm = new FormGroup({
+      email: this.email,
+      password: this.password,
+      name: this.name,
+      gender: this.gender,
+      birthday: this.birthday
+    });
   }
 
   onSubmit() {
-    console.log(this.user);
+    if(this.registerForm.valid){
+      this.user = this.registerForm.value;
+      console.log(this.user);
+    }
   }
 
 }
