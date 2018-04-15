0015cdc76e9707fd1b2dc39b84415d9bf5fe2125
Michael Sober <michael.sober@ymail.com>
2018-04-15 19:21:11 +0200

    Removed about component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { RegisterComponent } from './register/register.component';
@@ -21,7 +20,6 @@ import {ResearchfacilityGuard} from "./guard/researchfacility.guard";
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
     HomeComponent,
     RegisterComponent,
     LoginComponent
