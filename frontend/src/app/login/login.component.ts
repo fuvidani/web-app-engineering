203ad96b015b39f073b9f37747b9898428223de1
Michael Sober <michael.sober@ymail.com>
2018-04-16 22:41:35 +0200

    Refactored auth service and login component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Authentication} from "../model/authentication";
 import {AuthService} from "../service/auth.service";
+import {AccessToken} from "../model/accesstoken";
+import {Router} from "@angular/router";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -11,12 +14,13 @@ import {AuthService} from "../service/auth.service";
 export class LoginComponent implements OnInit {
 
   authentication: Authentication;
+  error: boolean = false;
 
   loginForm: FormGroup;
   email: FormControl;
   password: FormControl;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.authentication = new Authentication();
@@ -30,9 +34,33 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     if(this.loginForm.valid){
-     this.authentication = this.loginForm.value;
-     this.authService.login(this.authentication);
+      this.authentication = this.loginForm.value;
+      this.authService.authenticate(this.authentication).subscribe(
+        accessToken => {
+          this.handleSuccessFullAuthentication(accessToken);
+        },
+        err => {
+          this.handleFailedAuthentication(err);
+        },
+        () => {
+          this.handleFinishedAuthentication();
+        }
+      );
     }
+  }
+
+  handleSuccessFullAuthentication(accessToken: AccessToken) {
+    this.authService.setAccessToken(accessToken.token);
+    this.router.navigate(['/']);
+  }
+
+  handleFailedAuthentication(errorResponse: HttpErrorResponse) {
+    console.error(errorResponse);
+    this.error = true;
+  }
+
+  handleFinishedAuthentication() {
+    console.log("Successfully logged in.")
   }
 
 }
