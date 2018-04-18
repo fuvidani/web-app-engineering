8f40d532254f543a12f25e311c172c008699331c
Michael Sober <michael.sober@ymail.com>
2018-04-18 18:52:59 +0200

    Added header

--- frontend/src/app/medicalquery/medicalquery.component.ts
+++ frontend/src/app/medicalquery/medicalquery.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../service/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-medicalquery',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicalqueryComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.email = this.authService.getPrincipal();
+  }
+
+  scroll(elememt) {
+    elememt.scrollIntoView({behavior:'smooth'});
+  }
+
+  logOut() {
+    this.authService.clearAccessToken();
+    this.router.navigate(['/']);
   }
 
 }
