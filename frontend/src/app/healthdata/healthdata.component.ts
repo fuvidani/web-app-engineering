4dfbc189003e3235b8ae35900a5c60bfb1994ed7
David Molnar <david.bese.molnar@gmail.com>
2018-04-18 16:23:03 +0200

    bind logout to healthdata component; layout improvements

--- frontend/src/app/healthdata/healthdata.component.ts
+++ frontend/src/app/healthdata/healthdata.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HealthdataService} from '../service/healthdata.service';
 import {HealthData} from '../model/healthdata';
+import {AuthService} from '../service/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-healthdata',
@@ -9,16 +11,23 @@ import {HealthData} from '../model/healthdata';
 })
 export class HealthdataComponent implements OnInit {
   healthData = [];
+  email = '';
 
-  constructor(private healthdataService: HealthdataService) { }
+  constructor(private healthdataService: HealthdataService, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.healthdataService.healthData.subscribe(res => this.healthData = res);
     this.healthdataService.changeHealthData(this.healthData);
+    this.email = this.authService.getPrincipal();
   }
 
   addHealthData() {
     //this.healthData.push(new HealthData());
     this.healthdataService.changeHealthData(this.healthData);
+  }
+
+  logOut() {
+    this.authService.clearAccessToken();
+    this.router.navigate(['/']);
   }
 }
