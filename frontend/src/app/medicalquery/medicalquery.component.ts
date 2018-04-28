3de071374be0253fbe0ed8c9c58b337a1bef6191
Michael Sober <michael.sober@ymail.com>
2018-04-28 12:08:50 +0200

    Created formwith missing age

--- frontend/src/app/medicalquery/medicalquery.component.ts
+++ frontend/src/app/medicalquery/medicalquery.component.ts
@@ -3,6 +3,10 @@ import {AuthService} from '../service/auth.service';
 import {Router} from '@angular/router';
 import {MedicalQuery} from '../model/medicalquery';
 import {MedicalqueryService} from '../service/medicalquery.service';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {MatChipInputEvent} from "@angular/material";
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
+import {Gender} from "../model/gender";
 
 @Component({
   selector: 'app-medicalquery',
@@ -13,16 +17,56 @@ export class MedicalqueryComponent implements OnInit {
 
   email: string;
   test = new MedicalQuery(2, 'TU Vienna', 'Abfrage 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 10, 50, 60, 'FEMALE', ['cancer', 'skin']);
+  genders = [
+    new Gender('1', 'Male', 'MALE'),
+    new Gender('2', 'Female', 'FEMALE')
+  ];
+
+
+  medicalQueryForm: FormGroup;
+  name: FormControl;
+  description: FormControl;
+  financialOffering: FormControl;
+  gender: FormControl;
+  tags: FormControl;
+
+  tagList;
+  separatorKeysCodes = [ENTER, COMMA];
 
   constructor(
     private authService: AuthService,
     private medicalQueryService: MedicalqueryService,
     private router: Router
   ) {
+    this.name = new FormControl(
+      '',
+      [Validators.required]
+    );
+    this.description = new FormControl(
+      '',
+      [Validators.required]);
+    this.financialOffering = new FormControl(
+      '',
+      [Validators.required, Validators.min(0)]);
+    this.gender = new FormControl(
+      '',
+      [Validators.required]
+    );
+    /*this.tags = new FormControl(
+      '',
+      [Validators.required]);*/
+    this.medicalQueryForm = new FormGroup({
+      name: this.name,
+      description: this.description,
+      financialOffering: this.financialOffering,
+      //tags: this.tags,
+      gender: this.gender
+    });
   }
 
   ngOnInit() {
     this.email = this.authService.getPrincipal().email;
+    this.tagList = [];
   }
 
   scroll() {
@@ -37,6 +81,34 @@ export class MedicalqueryComponent implements OnInit {
 
   navigateToSharedHealthdata(qid: string) {
     this.router.navigate(['medicalquery', qid, 'shared']);
+  }
+
+  onSubmit() {
+    if (this.medicalQueryForm.valid) {
+      console.log("Medicalquery form is valid. Can be submitted.")
+      console.log(this.medicalQueryForm.value);
+    }
+  }
+
+  addTag(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.tagList.push({name: value.trim()});
+    }
+
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeTag(tag: string): void {
+    const index = this.tagList.indexOf(tag);
+
+    if (index >= 0) {
+      this.tagList.splice(index, 1);
+    }
   }
 
 }
